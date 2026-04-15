e54d8de33ae5d98feed3cfaa2853c14c3842ff5b
kubrataj2005-hue <kubrataj2005@gmail.com>
2026-04-15 10:50:54 +0530

    Create Appy1.py

---
+++ Appy1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_config("PragyaAI Content Generator", layout="wide")
+st.title("PragyaAI - Content Generator")
+st.image("PragyanAI_Transparent.jpg")
